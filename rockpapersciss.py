001a726d5c3a21fca9c9928de4db15534ca53b5a
hunnypotpy <51973728+hunnypotpy@users.noreply.github.com>
2021-03-09 12:55:47 -0800

    Outline of R/P/S

---
+++ rockpapersciss.py
@@ -0,0 +1,21 @@
+# welcome player
+
+#ask player input for rock, paper scissors
+
+#assign rock, paper, scissors to int 
+
+#computer to choose at random (r, p, s)
+
+''' make a function that will determine
+    player == computer (draw)
+
+    player rock >= computer scissors
+    player rock =< computer paper
+
+    player scissors >= computer paper 
+    player scissors =< computer rock
+
+    player paper >= computer rock
+    player paper =< computer scissors"
+
+ask to play again?     
